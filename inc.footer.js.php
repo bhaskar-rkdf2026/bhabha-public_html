58d4c9b1c0e0bf2c793bbaf8859e4ab005b7c843
Megha <megharaikwar139@gmail.com>
2026-08-05 10:40:17 +0530

    console errors

--- inc.footer.js.php
+++ inc.footer.js.php
@@ -46,21 +46,3 @@ $( ".datepicker" ).datepicker({
 <!--Custom JavaScript--> 
 <script src="<?php echo URL_JS;?>custom.js?v=<?php echo time();?>"></script>
 
-
-
-<!-- WhatsHelp.io widget -->
-<!--
-<script type="text/javascript">
-    (function () {
-        var options = {
-            whatsapp: "+910000000000", // WhatsApp number
-            call_to_action: "Message us", // Call to action
-            position: "left", // Position may be 'right' or 'left'
-        };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-</script>
-<!-- /WhatsHelp.io widget -->
--- testingindex.php
+++ testingindex.php
@@ -170,24 +170,7 @@ if(is_array($department) && count($department)>0)
 <!--Bootstrap core JavaScript--> 
 <?php include('inc.footer.js.php');?>
 
-<!-- whatsapp chat  -->
 
-<!-- WhatsHelp.io widget -->
-
-<script type="text/javascript">
-    (function () {
-        var options = {
-            whatsapp: "+919039809598", // WhatsApp number
-            call_to_action: "Message us", // Call to action
-            position: "left", // Position may be 'right' or 'left'
-        };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-</script>
-<!-- /WhatsHelp.io widget -->
 
 
 <!-- massanger  -->
